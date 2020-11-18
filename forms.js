65d1af7210b9d12f899f7e73529028f2f5b2de08
Jeremy Ware <jeremy@epype.net>
2020-11-18 15:16:08 -0500

    fixing syntax

---
+++ forms.js
@@ -0,0 +1,81 @@
+const init = function () {
+ document.getElementById('button-next').addEventListener('click', reset);
+ document.getElementById('button-submit').addEventListener('click', send);
+}
+
+const reset = function (ev) {
+
+ ev.preventDefault();
+
+ document.getElementById('form-user').reset();
+
+}
+
+const send = function (ev) {
+ ev.preventDefault();
+ ev.stopPropagation();
+
+ let fails = validate();
+
+ if (fails.length === 0) {
+
+  document.getElementById('form-user').submit();
+ } else {
+  fails.forEach(function (obj) {
+   let field = document.getElementById(obj.input);
+   field.parentElement.classList.add('error');
+   field.parentElement.setAttribute('data-errormsg', obj.msg);
+  })
+ }
+}
+
+const validate = function (ev) {
+
+ let failures = [];
+
+ let chk = document.getElementById('input-');
+
+ if (!chk.checked) {
+  failures.push({
+   input: 'input-',
+   msg: 'Must complete submit.'
+  })
+ }
+
+
+ let select = document.getElementById('input-age');
+ if (select.selectedIndex === 0) {
+  failures.push({
+   input: 'input-age',
+   msg: 'Invalid Number'
+  })
+ }
+
+
+ let first = document.getElementById('input-first');
+ let password = document.getElementById('input-password');
+ let email = document.getElementById('input-email');
+ if (first.value === "") {
+  failures.push({
+   input: 'input-first',
+   msg: 'Required Field'
+  })
+ }
+ if (password.value === "" || password.value.length < 8) {
+  failures.push({
+   input: 'input-password',
+   msg: 'Must be at least 8 chars'
+  })
+ }
+ if (email.value === "") {
+  failures.push({
+   input: 'input-email',
+   msg: 'Required Field'
+  })
+ }
+
+ return failures;
+}
+
+
+document.addEventListener('DOMContentLoaded', init);
