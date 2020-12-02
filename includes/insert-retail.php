a79375c27c6085fa6a0ec570671b3c3884632f73
Jeremy Ware <jeremy@epype.net>
2020-12-02 10:13:07 -0500

    fixing bugs

--- includes/insert-retail.php
+++ includes/insert-retail.php
@@ -6,61 +6,13 @@ $companyStreetLine2 = $_POST['company-street-address-line-2'];
 $companyCity = $_POST['company-city'];
 $companyState = $_POST['company-state'];
 $companyZip = $_POST['company-zip'];
-$companyPhoneNumber = $_POST['company-phone-number'];
-$companywebsiteUrl = $_POST['company-website-url'];
-$companyFirstName = $_POST['contact-first-name'];
-$companyLastName = $_POST['contact-last-name'];
-$businessCoporationRadio = $_POST['business-corporation-radio"'];
-$businessLlcRadio = $_POST['business-llc-radio"'];
-$businessProprietorRadio = $_POST['business-proprietor-radio"'];
-$businessOtherRadio = $_POST['business-other-radio"'];
-$federalTaxId = $_POST['federal-tax-id'];
-$businessDateFormed = $_POST['date-business-formed'];
-$businessStateCreated = $_POST['business-state-created'];
-$ownerFirstName = $_POST['owner-information-first-name'];
-$ownerFirstName = $_POST['owner-information-last-name'];
-$ownerTitle = $_POST['owner-title'];
-$ownerAge = $_POST['owner-age'];
-$ownerSocial = $_POST['owner-social'];
-$ownerDateOfBirth = $_POST['owner-date-of-birth'];
-$ownerCompanyAddress = $_POST['owner-company-address'];
-$ownerCompanyAddressLine2 = $_POST['owner-company-address-line-2'];
-$ownerCompanyCity = $_POST['owner-company-city'];
-$ownerCompanyState = $_POST['owner-company-state'];
-$ownerCompanyZipcode = $_POST['owner-company-zip'];
-$ownerCompanyAddress = $_POST['owner-company-drivers-license'];
-$secondaryOwnersFirstName = $_POST['secondary-owner-first-name'];
-$secondaryOwnersFirstName = $_POST['secondary-owner-last-name'];
-$secondaryOwnerTitle = $_POST['secondary-owner-title'];
-$secondaryOwnerAge = $_POST['secondary-owner-age'];
-$secondaryOwnerSocial = $_POST['secondary-owner-social'];
-$secondaryOwnerAddress = $_POST['secondary-owner-address'];
-$secondaryOwnerAddressLine2 = $_POST['secondary-owner-address-line-2'];
-$secondaryOwnerCity = $_POST['secondary-owner-city'];
-$secondaryOwnerState = $_POST['secondary-owner-state'];
-$secondaryOwnerZip = $_POST['secondary-owner-zip'];
-$secondaryOwnerDateOfBirth = $_POST['secondary-owner-drivers-license'];
-$describeProductInfo = $_POST['describe-product-info'];
-$productAnnualVolume = $_POST['product-annual-volume'];
-$productAverageTicket = $_POST['product-average-ticket'];
-$productHighTicket = $_POST['product-high-ticket'];
-$productInfoMailTelephone = $_POST['product-mail-telephone'];
-$productInfoInternetEcommerce = $_POST['product-internet-ecommerce'];
-
-
-
-
-
-
-
-
 
 // connection to localhost
 // if (!empty($dbaUserName) || !empty($companyStreet) || !empty($companyStreetLine2) || !empty($companyCity) || !empty($companyState) || !empty($companyZip)) {
 //  $dbServername = "localhost";
 //  $dbUsername = "root";
 //  $dbPassword = "";
-//  $dbName = "";
+//  $dbName = "register";
 
 // connection to remote server
 if (!empty($dbaUserName) || !empty($companyStreet) || !empty($companyStreetLine2) || !empty($companyCity) || !empty($companyState) || !empty($companyZip)) {
@@ -73,11 +25,11 @@ if (!empty($dbaUserName) || !empty($companyStreet) || !empty($companyStreetLine2
  if (mysqli_connect_error()) {
   die('Connect Error(' . mysqli_connect_errno() . ')' . mysqli_connect_error());
  } else {
-  // $SELECT = "SELECT * From platinum_payments Where dbaUserName = ? Limit 1";
-  // $INSERT = "INSERT Into platinum_payments (dbaUserName, companyStreet, companyStreetLine2, companyState, companyCity, companyZip) values(?, ?, ?, ?, ?, ?)";
+  // $SELECT = "SELECT * From PlatinumPaymentsRetailInfo Where dbaUserName = ? Limit 1";
+  // $INSERT = "INSERT Into PlatinumPaymentsRetailInfo (dbaUserName, companyStreet, companyStreetLine2, companyState, companyCity, companyZip) values(?, ?, ?, ?, ?, ?)";
 
-  $SELECT = "SELECT dbaUserName From platinum_payments Where dbaUserName = ? Limit 1";
-  $INSERT = "INSERT Into platinum_payments (dbaUserName, companyStreet, companyStreetLine2, companyState, companyCity, companyZip) values(?, ?, ?, ?, ?, ?)";
+  $SELECT = "SELECT dbaUserName From PlatinumPaymentsRetailInfo Where dbaUserName = ? Limit 1";
+  $INSERT = "INSERT Into PlatinumPaymentsRetailInfo (dbaUserName, companyStreet, companyStreetLine2, companyState, companyCity, companyZip) values(?, ?, ?, ?, ?, ?)";
 
   $stmt = $conn->prepare($SELECT);
   $stmt->bind_param("s", $dbaUserName);
